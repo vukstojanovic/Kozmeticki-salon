da7f779df3f2b31bd7072c8014c3339691f2b4e3
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2023-10-08 00:27:47 +0200

    Add json-server library and mock data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
-import './App.css';
-import Admin from './Admin';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useState } from 'react';
-import Home from './Home';
-
+import "./App.css";
+import Admin from "./Admin";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Home from "./Home";
 
 function App() {
   const [isAuthenticated] = useState(true);
@@ -23,11 +22,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
