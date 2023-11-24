a4c7d67737969d8cd0d9827c040761e7af7fb3f6
petar-antic <antic.petar994@gmail.com>
2023-11-24 16:47:49 +0100

    chose staff component

--- client/src/services/index.tsx
+++ client/src/services/index.tsx
@@ -1,31 +1,41 @@
 import axiosInstance from "../config/axios";
 
 type Services = {
-    id: number;
-    category_id: number;
-    name: string;
-    time_in_minutes: number;
-    price: number;
-    workers_id: number[];
+  id: number;
+  category_id: number;
+  name: string;
+  time_in_minutes: number;
+  price: number;
+  workers_id: number[];
 };
 
 export type ServicesData = {
-    data: Services[];
+  data: Services[];
 };
 
 type Categories = {
-    id: number;
-    name: string
-}
+  id: number;
+  name: string;
+};
+
+type Workers = {
+  id: number;
+  name: string;
+  image: string;
+};
 
 type CategoriesData = {
-    data: Categories[]
-}
+  data: Categories[];
+};
+
+type WorkersData = {
+  data: Workers[];
+};
 
 const apiServices = {
-    getServices: () => axiosInstance.get<null, ServicesData>("/services"),
-    getCategories: () => axiosInstance.get<null, CategoriesData>("/categories")
-
+  getServices: () => axiosInstance.get<null, ServicesData>("/services"),
+  getCategories: () => axiosInstance.get<null, CategoriesData>("/categories"),
+  getWorkers: () => axiosInstance.get<null, WorkersData>("/workers"),
 };
 
 export default apiServices;
